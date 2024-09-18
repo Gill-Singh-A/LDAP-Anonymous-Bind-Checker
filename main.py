0c610e0f7a2b852a64d258c173f6ed9a75e1c24f
Gill-Singh-A <amansg22@iitk.ac.in>
2024-09-18 13:34:37 +0530

    Implemented Command-Line Arguments

--- main.py
+++ main.py
@@ -13,6 +13,9 @@ status_color = {
     ' ': Fore.WHITE
 }
 
+port = 389
+ssl = True
+
 def display(status, data, start='', end='\n'):
     print(f"{start}{status_color[status]}[{status}] {Fore.BLUE}[{date.today()} {strftime('%H:%M:%S', localtime())}] {status_color[status]}{Style.BRIGHT}{data}{Fore.RESET}{Style.RESET_ALL}", end=end)
 
@@ -23,4 +26,23 @@ def get_arguments(*args):
     return parser.parse_args()[0]
 
 if __name__ == "__main__":
-    pass
+    arguments = get_arguments(('-s', "--server", "server", "Target LDAP Servers (seperated by ',' or File Name containing Targets)"),
+                              ('-p', "--port", "port", f"Port of Target LDAP Servers (Default={port})"),
+                              ('-s', "--ssl", "ssl", f"Use SSL (True/False, Default={ssl})"),
+                              ('-w', "--write", "write", "CSV File to Dump Successful Logins (default=current data and time)"))
+    if not arguments.server:
+        display('-', f"Please specify {Back.YELLOW}Target Server{Back.RESET}")
+        exit(0)
+    else:
+        try:
+            with open(arguments.server, 'r') as file:
+                arguments.server = [server for server in file.read().split('\n') if server != '']
+        except FileNotFoundError:
+            arguments.server = arguments.server.split(',')
+        except Exception as error:
+            display('-', f"Error Occured while reading File {Back.MAGENTA}{arguments.server}{Back.RESET} => {Back.YELLOW}{error}{Back.RESET}")
+            exit(0)
+    arguments.port = port if not arguments.port else int(arguments.port)
+    arguments.ssl = False if arguments.ssl == "False" else ssl
+    if not arguments.write:
+        arguments.write = f"{date.today()} {strftime('%H_%M_%S', localtime())}.csv"
